db3a74efe5571ab130b4da25bf7c5ff2b08ea8b8
olkn <42804223+kotyaraa@users.noreply.github.com>
2020-04-09 18:54:44 +0300

    my first Header commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FuterComponent } from './futer/futer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// import {HttpClientModule} from '@angular/common/http';
+// import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+// import {MatNativeDateModule} from '@angular/material/core';
+// import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+// import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    
+    FuterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/futer/futer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-futer',
+  templateUrl: './futer.component.html',
+  styleUrls: ['./futer.component.scss']
+})
+export class FuterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
